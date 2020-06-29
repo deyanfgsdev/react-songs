4b3a8266d4a01bd57bb4b268abdafc37f21fcc20
Deyán G. Sabogal <deyanfabricioguacaransabogal@MacBook-Pro-de-Deyan-Fabricio-Guacaran-Sabogal.local>
2020-06-29 17:13:34 +0100

    Add connect Component (react-redux) to SongList Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SongList from "./SongList";
 
 const App = () => {
-  return <div>App!!</div>;
+  return (
+    <div className="app">
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
